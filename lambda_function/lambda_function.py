7ad84922d3f551105f3b89d1eebc319c8916f1d3
Wanjiru Murira <wanjirumurirah@gmail.com>
2023-09-29 14:20:11 +0300

    lambda function update

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event:any, context:any):
     
     #create a dynamodb table
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("visitor-count-table")
+    table = dynamodb.Table("visitor_counter")
  
      # Query the DynamoDB table to get the current maximum user ID
     response = table.scan(Select="COUNT")
@@ -20,22 +20,16 @@ def lambda_handler(event:any, context:any):
     time_visited = datetime.now(eat_timezone).strftime("%Y-%m-%d %H:%M")
 
     item = {
-        "user": "user"+ str(visit_count),
+        "user_id": "user"+ str(visit_count),
         "count": visit_count,
         "time": time_visited
     }
     
     #Put the new visit count into the table
     table.put_item(Item=item)
-<<<<<<< HEAD
-    #message = f"Hello!This page has been visited {visit_count} times." 
-    results = {"count": visit_count}
-    return json.dumps(results)
-=======
     message = f"Hello!This page has been visited {visit_count} times." 
     results = {"Message" : message,
             "count": visit_count}
     return results
->>>>>>> 56a9797cb2110ece6989ea08878b3293c436a47d
 
 #Compress-Archive -Path .\lambda_function.py, .\venv\lib\site-packages\* -DestinationPath my_lambda_function.zip
